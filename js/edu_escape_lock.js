b6682e962cb49a0c429cb74c6d37ee698bdeece1
Matthias <info@matthias-andrasch.de>
2018-02-21 16:50:08 +0100

    do not use modal anymore, small changes

--- js/edu_escape_lock.js
+++ js/edu_escape_lock.js
@@ -10,28 +10,29 @@ var AJAX_UNLOCK_URL = 'codes.json';
 
 
 var sendUnlockCode = function(code_input_from_user) {
-	$(".unlock-error-message").hide();
+    $(".unlock-error-message").hide();
     $.getJSON(AJAX_UNLOCK_URL, function(data) {
 
         if (data.codes.hasOwnProperty(code_input_from_user)) {
             var redirect_url = data.codes[code_input_from_user];
-            var redirect_message = '<p>You will be redirected automatically. If not - please click <a href="' + redirect_url + '">here</a></p>';
- 
-            $("#redirect-modal .modal-body").html(redirect_message);
-            $("#redirect-modal").modal();
+            var redirect_message = '<p><h3>Code unlocked!</h3> <a class="btn btn-primary" href="' + redirect_url + '">Open</a></p>';
 
+            //$("#redirect-modal .modal-body").html(redirect_message);
+            //$("#redirect-modal").modal();
             // Your application has indicated there's an error
-            window.setTimeout(function() {
+            /*window.setTimeout(function() {
                 window.open(redirect_url);
-            }, 1500);
+            }, 1500);*/
 
-            $('#pincode-input1').pincodeInput().data('plugin_pincodeInput').clear();
+            $(".unlock-success-message").html(redirect_message).show();
+
+            //$('#pincode-input1').pincodeInput().data('plugin_pincodeInput').clear();
 
         } else {
             // 2DO show error, code does not exist
             $(".unlock-error-message").fadeIn();
 
-          	
+
         }
     });
 
@@ -45,7 +46,12 @@ $(function() {
         digits: 4,
         complete: function(value, e, errorElement) {
             sendUnlockCode(value);
-        }
+        },
+        change: function(input, value, inputnumber) {
+            $(".unlock-error-message").hide();
+            $("unlock-successful-message").hide();
+
+         }
     });
 
     $('#pincode-input1').pincodeInput().data('plugin_pincodeInput').focus();
